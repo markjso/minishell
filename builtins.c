66c88cc7aa73010885466b97e1ea018fd42ee995
Joanne Marks <jmarks@F2N062.42adel.org.au>
2023-05-09 13:37:04 +0930

    finished echo

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 /*   By: jmarks <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 13:55:05 by jmarks            #+#    #+#             */
-/*   Updated: 2023/05/01 16:11:16 by jmarks           ###   ########.fr       */
+/*   Updated: 2023/05/08 16:46:08 by jmarks           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,52 @@ void	printpwd(void)
 	printf("\nDir: %s", pwd);
 }
 
+void	get_env(char **envp)
+{
+    int i = 0;
+
+    while (envp[i]!= NULL)
+	{
+        printf("%s\n", envp[i]);
+        i++;
+    }
+}
+
+void echo_cmd(char **token)
+{
+    int	i;
+    int	no_newline; // flag for -n option
+
+	i = 1;
+	no_newline = 0;
+    // check if -n option was passed
+    if (token[i] && ft_strcmp(token[i], "-n") == 0)
+    {
+        no_newline++;
+        i++;
+    }
+    while (token[i])
+    {
+        if (token[i] && ft_strcmp(token[i], "-n") == 0)
+		{
+		printf("%s", token[i]);
+        if (token[i + 1])
+            printf(" ");
+		}
+        i++;
+    }
+    // print new line only if -n option was not passed and there are arguments
+    if (!token[1] && no_newline)
+        printf("\n");
+}
+
 int	builtins(char **token)
 {
 	int		no_of_builtins;
 	int		i;
-	char	*builtin_ID[no_of_builtins];
-
+	
 	no_of_builtins = 7;
+	char	*builtin_ID[no_of_builtins];
 	i = 0;
 	builtin_ID[0] = "echo";
 	builtin_ID[1] = "cd";
@@ -39,8 +78,14 @@ int	builtins(char **token)
 	{
 		i++;
 	}
-	if (i == 2)
+	if (i == 0)
+		echo_cmd(token + 1);
+	else if (i == 1)
+		cd_command(token);
+	else if (i == 2)
 		printpwd();
+	else if (i == 5)
+		get_env(token);
 	else if (i == 6)
 		exit(0);
 	return (0);
@@ -51,8 +96,8 @@ void	parse_input(char *str, char **token)
 	int		i;
 	char	**split;
 
-	split = ft_split(str, ' ');
-	if (!split)
+	split = ft_split(str, ' '); // Returns MALLOC string
+	if (!split) // When does this get called?
 	{
 		printf("empty string\n");
 	}
