f03f497b71154d38909e84f68efc547b4283f8ad
Joanne Marks <jmarks@F2N062.42adel.org.au>
2023-05-09 14:07:00 +0930

    added ft_strcmp

--- utils.c
+++ utils.c
@@ -38,3 +38,15 @@ void	ft_free_array(char **arr)
 	}
 	free(arr);
 }
+
+int 	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
