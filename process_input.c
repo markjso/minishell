26f2e58bb7c37f90b0bb533005d2c485d715eec8
Jack Church <jchurch@F2N036.42adel.org.au>
2023-06-14 15:49:24 +0930

    End of day. Look in process_input for your current todo. Must move expand_input after tokenised string.

--- process_input.c
+++ process_input.c
@@ -36,6 +36,38 @@ void	parse_input(char *str, char **token)
 	free(split);
 }
 
+
+    // t_envar *pwd;
+    // t_envar *oldpwd;
+
+    // pwd = find_env_var("PWD");
+
+void expand_input(char *str)
+{
+	int i;
+	int len;
+	char *variable;
+	t_envar	*env_var;
+
+	len = 0;
+	// WHILE the string exists, increment through each char index. 
+	while (str[i] != '\0')
+	{
+		// IF current char is $ and next is alpha number, there is the potential for this to be an env var. 
+		if (str[i] == '$' && ft_isalnum(str[i + 1] == 1))
+		{
+			input
+			// Get the lenth of the potential env var. 
+			len = ft_alnum_word_len(str, i);
+
+			env_var = find_env_var(*token);
+
+		}
+		// else if (str[i] == '$' && str[i + 1] == '\0'){ print dollar sign if echo $ }
+		i++;
+	}
+}
+
 /*
 process_input(char *str: raw user input, char **token: is empty pointer to string that will become tokenised string).
 Return value is always 0? Why
@@ -43,6 +75,7 @@ Return value is always 0? Why
 int	process_input(char *str, char **token)
 {
 	debugFunctionName("PROCESS_INPUT");
+	expand_input(str);
 	parse_input(str, token);
 	// tampvar is just for debugging purposes. Can cahange back to `if (builitins(token))`. 
 	// if (builtins(token));
