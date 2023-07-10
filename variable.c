cbfffca85b1f21598506317b8dd27c4bf26bf0b5
Joanne Marks <jmarks@F2N062.42adel.org.au>
2023-07-10 13:48:36 +0930

    Look in jack notes for todo

--- variable.c
+++ variable.c
@@ -57,25 +57,33 @@ void	skip_single_quote(char *src, int *end)
 
 void	dollar_found(t_token_list **root, t_token_list *curr, t_token_list *new_node, int *end, int *start)
 {
-	char	*temp;
-	char	*env_exp;
+	char	*first;
+	char	*env_str;
+	int		env_len;
+	int		env_start;
+
+	env_start = *end;
+	while (ft_is_not_white_space(curr->data[*end] && curr->data[*end] != '\0')
+		(*end)++;
+	env_str = expand_dollar(ft_substr(curr->data, env_start, *end - env_start));
 
 	*end = env_len(curr->data);
-	temp = ft_substr(curr->data, *start, *end - *start);
-	env_exp = expand_dollar(ft_substr(curr->data, *start, *end - *start));
+	first = ft_substr(curr->data, *start, env_start;
 	new_node = make_new_node(ft_strjoin(temp, env_exp));
 	ll_insert_before(root, curr, new_node);
 	*start = *end;
 }
 
-void	maybe_expand(t_token_list **root, t_token_list *curr)
+void	maybe_expand(t_token_list **root, t_token_list *curr, int *join)
 {
 	int	end;
 	int	start;
 	t_token_list	*new_node;
+	t_token_list	*join_1_node;
 
 	end = 0;
 	start = 0;
+	*join = 0;
 	while(curr->data[end] != '\0')
 	{
 		if (curr->data[end] == 39) // If is '. Skip over until next ' is found or end of string. 
@@ -90,6 +98,12 @@ void	maybe_expand(t_token_list **root, t_token_list *curr)
 		}
 		while (curr->data[end] != '\0' && curr->data[end] != '$' && curr->data[end] != 39)
 			end++;
+		if (curr->data[end] == "$") // If next is $
+		{
+			*join = 1;
+			join_1_node = curr;
+			dollar_found(root, curr, new_node, &end, &start);
+		}
 		new_node = make_new_node((ft_substr(curr->data, start, end - start)));
 		ll_insert_before(root, curr, new_node);
 		start = end;
@@ -125,11 +139,12 @@ void    expand_tokens(t_token_list **root)
 {
 	t_token_list	*curr;
 	t_token_list	*temp;
+	int				join;
 
 	curr = *root;
 	while (curr != NULL)
 	{
-		maybe_expand(root, curr);
+		maybe_expand(root, curr, &jion);
 		temp = curr;
 		curr = curr->next;
 		ll_remove_node(root, temp);
