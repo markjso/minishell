537449c8676b76074c9634aad84d995239ea0111
Joanne Marks <jmarks@F2N062.42adel.org.au>
2023-08-09 15:58:22 +0930

    Aidan

--- pipes.c
+++ pipes.c
@@ -21,7 +21,7 @@ void exepipe(void)
 	char **paths;
 	char *exec_path;
     char *cmds;
-    int status;
+    // int status;
 
 	cmds = g_program.token[0];
 	printf("cmd %s\n", cmds);
@@ -29,16 +29,21 @@ void exepipe(void)
 	exec_path = get_path_for_cmd(paths, cmds);
 	if (exec_path)
 	{
-		execve(exec_path, g_program.commands, g_program.envp);
+		printf("Path = %s\n", exec_path);
+		for (int i = 0; g_program.commands[i]; i++)
+		{
+			printf("arg[%i] = %s\n", i, g_program.commands[i]);
+		execve(exec_path, &g_program.commands[i], g_program.envp);
+		}
 		perror("execve"); 
 		exit(EXIT_FAILURE); 
 	}
-	waitpid(g_program.pid, &status, 0);
-	if (WIFEXITED(status))
-	{
-		g_program.exit_status = WEXITSTATUS(status);
-		ft_free_array(g_program.commands);
-	}
+	// waitpid(g_program.pid, &status, 0);
+	// if (WIFEXITED(status))
+	// {
+	// 	g_program.exit_status = WEXITSTATUS(status);
+	// 	ft_free_array(g_program.commands);
+	// }
 	exit(EXIT_SUCCESS); // Ensure the child process exits after executing the command.
 }
 
@@ -71,61 +76,42 @@ void	set_commands(void)
 	}
 }
 
-/*
-	first cmd will be first token 
-	2nd cmd will be immeditay after pipe 
-*/
-
 void do_pipe(void)
 {
     debugFunctionName("DO_PIPE");
 
     int pipe1[2];
 	pid_t	pid;
-	pid_t	pid2;
 	int status;
+
     if (pipe(pipe1) == -1)
 	{
 		perror("bad pipe");
 		exit(1);
 	}
     pid = fork();
-	if (pid == 0)
+	if (pid)
 	{
-		dup2(pipe1[1], STDOUT_FILENO);
-		close(pipe1[0]);
 		close(pipe1[1]);
-		printf("getting to child\n");
-		exepipe();
+		dup2(pipe1[0], 0);
+		wait(&status);
+		if (WEXITSTATUS(status) != EXIT_SUCCESS)
+			printf("Failed\n");
 	}
 	else
 	{
-		pid2=fork();
-		if (pid2 == 0)
-		{
-			dup2(pipe1[0], STDIN_FILENO);
-			close(pipe1[1]);
-			close(pipe1[0]);
-			printf("getting to parent\n");
-			exepipe();	
-		}
-		else
-		{
-			close(pipe1[0]);
-			close(pipe1[1]);
-			waitpid(pid2, &status, 1);
-		}
-		waitpid(pid, &status, 1);
+		close(pipe1[0]);
+		dup2(pipe1[1], 1);
+		exepipe();
 	}
 }
-
 
 void	execute_commands(void)
 {
 	 debugFunctionName("EXEXUTE_PIPE_COMMAND");
     //  int	i = 0;
 
-	sig_initialiser();
+	// sig_initialiser();
 	set_commands();
 	// printf("commmands in g_program.commands: %s\n", g_program.commands[i]);
 	do_pipe();
